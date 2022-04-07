27197de6641fe19f059da6d6d584f6746e54d485
Blank217 <99425675+aidan215@users.noreply.github.com>
2022-04-07 13:58:08 +0800

    Updates on commser.cpp and Distribution.md and created main.cpp
    create helping chruch in commser.cpp and update Distribution.md for clarifying what details will the Charater user have.
    
    Also, created a main.cpp to hold the struct of character and some brief guideline on the story /functions flow.

--- commser.cpp
+++ commser.cpp
@@ -2,6 +2,9 @@
 #include "commser.h"
 #include <iostream>
 #include <cstdlib>
+#include <unistd.h>
+
+using namespace std;
 
 int main(){
     // need 5 community service
@@ -9,7 +12,45 @@ int main(){
     // 1) helping chruch
         // take care of kids
         // nurse elderly
+    
+    // goal to increase luck and exp
+    void helpingChruch( Character user){
+        cout << "You have been recently assign to help out with the Chruch" << endl;
+        usleep(1000000);
+        cout << "You are task with helping nurse the elderly" << endl;
+        usleep(1000000);
+        // User option
+        cout << "Would you like to participate?" << endl;
+        usleep(1000000);
         
+
+        string reply = "";
+        while(reply != "Yes" || reply != "No"){
+           cout << "Yes / No: ";
+           cin >> reply;
+           if (reply != "Yes" || reply != "No"){
+               cout << "Please input Yes or No" << end;
+           } 
+        }
+
+        if(reply == "Yes"){
+            cout << "Great Choice!" << endl;
+            usleep(1000000);
+            cout << " You have officially been task with helping the elderly" << endl;
+            for(int i = 1; i<4; i++){
+                cout << "Helping..." << " " << std::flush;
+                int sleeping = 100000*i;
+                usleep(sleeping);
+            }
+            user.experience++; // can be adjusted
+            user.luck++;
+        }
+        
+        // why is there an error for my else if say that expected a declaration??
+        else if(reply == "No"){
+            cout << "Alright!" << endl;
+        }
+    }
 
 
     // 2) helping with food shortage
@@ -30,4 +71,5 @@ int main(){
         // notice a kid fall down
         // elderly tired and looking for direction
         // foreigners causing troubles
+    return 0;
 }
